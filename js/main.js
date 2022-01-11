47bf8b4e6969bfbb293f9ef0f5b11f02f96ffa73
DT <54771496+dt-japanscript@users.noreply.github.com>
2022-01-11 15:40:01 +0900

    Create and style headerFixed

--- js/main.js
+++ js/main.js
@@ -7,25 +7,32 @@ var $grid = $('.portfolio').isotope({
     layoutMode: 'fitRows',
   });
 
-  // bind filter button click
-  $('#filters').on( 'click', 'button', function() {
-    var filterValue = $( this ).attr('data-filter');
-    // use filterFn if matches value
-    filterValue = filterValue;
-    $grid.isotope({ filter: filterValue });
-  });
+// bind filter button click
+$('#filters').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  // use filterFn if matches value
+  filterValue = filterValue;
+  $grid.isotope({ filter: filterValue });
+});
 
-  // change is-checked class on buttons
-  $('.button-group').each( function( i, buttonGroup ) {
-    var $buttonGroup = $( buttonGroup );
-    $buttonGroup.on( 'click', 'button', function() {
-      $buttonGroup.find('.is-checked').removeClass('is-checked');
-      $( this ).addClass('is-checked');
-    });
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
   });
+});
 
 
-  // venobox
-  new VenoBox({
-    selector: '.venobox'
-  });
+// venobox
+new VenoBox({
+  selector: '.venobox'
+});
+
+// Header scroll handling
+window.onscroll = function(){
+  if(document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
+    // document.getElementById("headerTop").style.transform = "translate(-50%,0)";
+  }
+}
