8c35c1f13f8f95cb9623372a22ecf81f49436c66
Do Tung <54771496+dt-japanscript@users.noreply.github.com>
2022-01-13 09:42:36 +0900

    Linting

--- js/main.js
+++ js/main.js
@@ -3,24 +3,24 @@
 
 // init Isotope
 var $grid = $('.portfolio').isotope({
-    itemSelector: '.portfolio-item',
-    layoutMode: 'fitRows',
-  });
+  itemSelector: '.portfolio-item',
+  layoutMode: 'fitRows',
+});
 
 // bind filter button click
-$('#filters').on( 'click', 'button', function() {
-  var filterValue = $( this ).attr('data-filter');
+$('#filters').on('click', 'button', function () {
+  var filterValue = $(this).attr('data-filter');
   // use filterFn if matches value
   filterValue = filterValue;
   $grid.isotope({ filter: filterValue });
 });
 
 // change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
+$('.button-group').each(function (i, buttonGroup) {
+  var $buttonGroup = $(buttonGroup);
+  $buttonGroup.on('click', 'button', function () {
     $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
+    $(this).addClass('is-checked');
   });
 });
 
@@ -31,8 +31,63 @@ new VenoBox({
 });
 
 // Header scroll handling
-window.onscroll = function(){
-  if(document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
+window.onscroll = function () {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
     // document.getElementById("headerTop").style.transform = "translate(-50%,0)";
   }
 }
+
+// Owl Carousel
+$('.testimonial__carousel').owlCarousel({
+  loop: true,
+  nav: false,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 600,
+  navSpeed: 100,
+  responsive: {
+    0: {
+      items: 3,
+    },
+    1200: {
+      items: 1,
+    },
+  }
+});
+$('.clients__carousel').owlCarousel({
+  loop: true,
+  nav: false,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 600,
+  navSpeed: 100,
+  responsive: {
+    0: {
+      items: 3,
+    },
+  }
+});
+$('.portfolio-img-7-carousel').owlCarousel({
+  loop: true,
+  nav: false,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 600,
+  responsive: {
+    0: {
+      items: 1,
+    },
+  }
+});
+$('.portfolio-img-8-carousel').owlCarousel({
+  loop: true,
+  nav: false,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 600,
+  responsive: {
+    0: {
+      items: 1,
+    },
+  }
+});
